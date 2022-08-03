fbfd68a4909912a42fee46ac5e9990be8add4031
Max Hofer <mh182@chello.at>
2022-08-03 18:32:39 +0200

    Move containers into a pod

--- load-migrator.sh
+++ load-migrator.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-CORE_DB_PASSWORD=${CORE_DB_PASSWORD:-postgres}
 VERSION=v3.9.0
+POD_NAME=migrator
+CORE_DB_PASSWORD=${CORE_DB_PASSWORD:-postgres}
 
 echo "Cleaning up exiting containers"
-podman stop core && podman rm core
-podman stop core_db && podman rm core_db
+podman pod stop ${POD_NAME} && podman pod rm ${POD_NAME}
+
+# For now lets expose internal ports
+podman pod create --name migrator -p 5432:5432 -p 3000:3000
+echo "Created pod '${POD_NAME}'"
 
 # This will create a volume container called 'core_db-data'
 # TODO: Why we need :Z option for the read-only file 'initdb.sh'? If ommitted the file is not mounted into the container
-podman run -d --name core_db -p 5432:5432 \
+echo "Starting core_db"
+podman run -d --pod ${POD_NAME} --name core_db \
     -v ./docker/initdb.sh:/docker-entrypoint-initdb.d/init-user-db.sh:Z \
     -v core_db-data:/var/lib/postgresql/data:Z \
     -e POSTGRES_PASSWORD=${CORE_DB_PASSWORD} \
@@ -18,8 +23,9 @@ podman run -d --name core_db -p 5432:5432 \
 echo "Waiting until database is up"
 sleep 5
 
-podman run -d --name core -p 3000:3000\
-    -e CORE_DB_HOST=core_db \
+echo "Starting core"
+podman run -d --pod ${POD_NAME} --name core \
+    -e CORE_DB_HOST=localhost \
     -e CORE_DB_PASSWORD=${CORE_DB_PASSWORD} \
     -e CORE_DB_DATABASE=migrator \
     docker.io/cybertecpostgresql/cybertec_migrator-core:${VERSION}
